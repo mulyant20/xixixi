53104b328c9e0265a30c7dc3f177d69b2984bae7
mulyant20 <mulyan.t20@gmail.com>
2023-03-06 08:58:10 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,38 @@
 import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { fetchJokes } from './store/slice/jokesSlice'
+import { closeModal, fetchJokes, fetchRandomJokes } from './store/slice/jokesSlice'
 
 function App() {
   const dispatch = useDispatch()
-  const {data, isLoading, err} = useSelector((store) => store?.jokes)
+  const { data, isLoading, err, joke, isOpen } = useSelector(
+    (store) => store?.jokes
+  )
+
   useEffect(() => {
     dispatch(fetchJokes())
   }, [])
 
-  if(isLoading) {
+  const generate = () => {
+    dispatch(fetchRandomJokes())
+  }
+
+  if (isLoading) {
     return <p>Loading...</p>
   }
-  
+
+  if (err) {
+    return <p>Error</p>
+  }
+
   return (
     <div className='App'>
+      {isOpen && (
+        <div>
+          <p>{joke}</p>
+          <button onClick={() => dispatch(closeModal())}>x</button>
+        </div>
+      )}
+      <button onClick={generate}>Generate</button>
       <ul>
         {data.map((j, index) => (
           <li key={index}>{j}</li>
--- src/store/slice/jokesSlice.js
+++ src/store/slice/jokesSlice.js
@@ -5,19 +5,39 @@ const BASE_URL = 'https://candaan-api.vercel.app/api'
 
 const initialState = {
   data: [],
+  joke: null,
   isLoading: true,
+  isOpen: false,
   err: false,
 }
 
 export const fetchJokes = createAsyncThunk('jokes/fetchJokes', async () => {
-  const res = await fetch('https://candaan-api.vercel.app/api/text')
+  const res = await fetch(`${BASE_URL}/text`)
   const data = await res.json()
   return data.data
 })
 
+export const fetchRandomJokes = createAsyncThunk(
+  'jokes/fetchRandomJokes',
+  async () => {
+    const res = await fetch(`${BASE_URL}/text/random`)
+    const data = await res.json()
+    return data.data
+  }
+)
+
 const jokesSlice = createSlice({
   name: 'jokes',
   initialState,
+  reducers: {
+    closeModal: (state, action) => {
+      state.joke = null
+      state.isOpen = false
+    },
+    openModal: (state, action) => {
+      state.isOpen = true
+    },
+  },
   extraReducers: (builder) => {
     builder.addCase(fetchJokes.pending, (state, action) => {
       state.isLoading = true
@@ -31,7 +51,21 @@ const jokesSlice = createSlice({
       state.isLoading = false
       state.err = true
     })
+    builder.addCase(fetchRandomJokes.pending, (state, action) => {
+      // state.isLoading = true
+    })
+    builder.addCase(fetchRandomJokes.fulfilled, (state, action) => {
+      state.joke = action.payload
+      state.isLoading = false
+      state.isOpen = true
+    })
+    builder.addCase(fetchRandomJokes.rejected, (state, action) => {
+      state.joke = null
+      state.isLoading = false
+      state.err = true
+    })
   },
 })
 
+export const { closeModal, openModal } = jokesSlice.actions
 export default jokesSlice.reducer
